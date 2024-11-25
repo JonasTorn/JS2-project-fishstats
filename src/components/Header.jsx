ed837b345a4037ba8e303a46fe455ee77b891f6b
Igelkotte <jonas.toern@gmail.com>
2024-11-25 14:52:35 +0100

    Fixat lite design och layout i header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -26,56 +26,70 @@ const Header = () => {
 				/>
 			</span>
 			
-			{/* <Sheet>
+			<Sheet>
 				<SheetTrigger asChild>
-					<Button variant="outline">Open</Button>
+					<Button variant="outline">MENY</Button>
 				</SheetTrigger>
-				<SheetContent>
-					<SheetHeader>
-						<SheetTitle>Edit profile</SheetTitle>
-						<SheetDescription>
+				<SheetContent  className="px-0 flex flex-col gap-0 items-stretch">
+					<SheetHeader  className="px-8 py-4 place-self-center">
+						<SheetTitle><h4>Meny</h4></SheetTitle>
+						{/* <SheetDescription>
 							Make changes to your profile here. Click save when you're done.
-						</SheetDescription>
+						</SheetDescription> */}
 					</SheetHeader>
-					<div className="grid gap-4 py-4">
-						<div className="grid grid-cols-4 items-center gap-4">
-							<Label htmlFor="name" className="text-right">
-								Name
-							</Label>
-							<Input id="name" value="Pedro Duarte" className="col-span-3" />
-						</div>
-						<div className="grid grid-cols-4 items-center gap-4">
-							<Label htmlFor="username" className="text-right">
-								Username
-							</Label>
-							<Input id="username" value="@peduarte" className="col-span-3" />
-						</div>
-					</div>
-					<SheetFooter>
+					<NavLink to="/" className="hover:text-muted-foreground ">
+					<div className="size-full px-8 py-4 hover:bg-secondary hover:text-background">START</div>
+					</NavLink>
+
+					<Separator className="auto " />
+
+					<NavLink to="/fiske" className="hover:text-muted-foreground ">
+					<div className="size-full px-8 py-4 hover:bg-secondary hover:text-background">FISKE</div>
+					</NavLink>
+
+					<Separator className="w-auto" />
+
+					<NavLink to="/statistik" className="hover:text-muted-foreground ">
+						<div className="size-full px-8 py-4 hover:bg-secondary hover:text-background">STATISTIK</div>
+					</NavLink>
+
+					<Separator className="w-auto" />
+
+					<NavLink to="/boende" className="hover:text-muted-foreground ">
+					<div className="size-full px-8 py-4 hover:bg-secondary hover:text-background">BOENDE</div>
+					</NavLink>
+
+					<Separator className="w-auto" />
+
+					<NavLink to="/bilder" className="hover:text-muted-foreground ">
+					<div className="size-full px-8 py-4 hover:bg-secondary hover:text-background">BILDER</div>
+					</NavLink>
+					
+					<SheetFooter className="place-self-center w-[50%] my-4">
 						<SheetClose asChild>
-							<Button type="submit">Save changes</Button>
+							<Button className="w-full bg-secondary">Stäng</Button>
 						</SheetClose>
 					</SheetFooter>
 				</SheetContent>
-			</Sheet> */}
+			</Sheet>
 			<nav>
 				<div className="flex space-x-4 text-background text-xl">
 					<NavLink to="/" className="hover:text-muted-foreground ">
 						START
 					</NavLink>
-					<Separator orientation="vertical" className="h-6 self-center" />
+					<Separator orientation="vertical" className="h-6 self-center bg-muted-foreground" />
 					<NavLink to="/fiske" className="hover:text-muted-foreground ">
 						FISKE
 					</NavLink>
-					<Separator orientation="vertical" className="h-6 self-center" />
+					<Separator orientation="vertical" className="h-6 self-center bg-muted-foreground" />
 					<NavLink to="/statistik" className="hover:text-muted-foreground ">
 						STATISTIK
 					</NavLink>
-					<Separator orientation="vertical" className="h-6 self-center" />
+					<Separator orientation="vertical" className="h-6 self-center bg-muted-foreground" />
 					<NavLink to="/boende" className="hover:text-muted-foreground ">
 						BOENDE
 					</NavLink>
-					<Separator orientation="vertical" className="h-6 self-center" />
+					<Separator orientation="vertical" className="h-6 self-center bg-muted-foreground" />
 					<NavLink to="/bilder" className="hover:text-muted-foreground ">
 						BILDER
 					</NavLink>
