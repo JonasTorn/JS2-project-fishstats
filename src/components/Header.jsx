0b71eda0935350b19acc93f13a900cbcba7b5f7c
Igelkotte <jonas.toern@gmail.com>
2024-11-25 16:00:48 +0100

    Flyttade hamburgermenyn till egen komponent

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,7 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-import { Separator } from "./ui/separator";
-import {
-	Sheet,
-	SheetClose,
-	SheetContent,
-	SheetDescription,
-	SheetFooter,
-	SheetHeader,
-	SheetTitle,
-	SheetTrigger,
-} from "@/components/ui/sheet";
-import { Button } from "./ui/button";
-import { Label } from "./ui/label";
-import { Input } from "./ui/input";
+
+import HamburgerMenu from "./HamburgerMenu";
+import NavLinks from "./NavLinks";
 
 const Header = () => {
 	return (
@@ -25,74 +13,11 @@ const Header = () => {
 					className="w-auto"
 				/>
 			</span>
-			
-			<Sheet>
-				<SheetTrigger asChild>
-					<Button variant="outline">MENY</Button>
-				</SheetTrigger>
-				<SheetContent  className="px-0 flex flex-col gap-0 items-stretch">
-					<SheetHeader  className="px-8 py-4 place-self-center">
-						<SheetTitle><h4>Meny</h4></SheetTitle>
-						{/* <SheetDescription>
-							Make changes to your profile here. Click save when you're done.
-						</SheetDescription> */}
-					</SheetHeader>
-					<NavLink to="/" className="hover:text-muted-foreground ">
-					<div className="size-full px-8 py-4 hover:bg-secondary hover:text-background">START</div>
-					</NavLink>
 
-					<Separator className="auto " />
-
-					<NavLink to="/fiske" className="hover:text-muted-foreground ">
-					<div className="size-full px-8 py-4 hover:bg-secondary hover:text-background">FISKE</div>
-					</NavLink>
-
-					<Separator className="w-auto" />
-
-					<NavLink to="/statistik" className="hover:text-muted-foreground ">
-						<div className="size-full px-8 py-4 hover:bg-secondary hover:text-background">STATISTIK</div>
-					</NavLink>
-
-					<Separator className="w-auto" />
-
-					<NavLink to="/boende" className="hover:text-muted-foreground ">
-					<div className="size-full px-8 py-4 hover:bg-secondary hover:text-background">BOENDE</div>
-					</NavLink>
-
-					<Separator className="w-auto" />
-
-					<NavLink to="/bilder" className="hover:text-muted-foreground ">
-					<div className="size-full px-8 py-4 hover:bg-secondary hover:text-background">BILDER</div>
-					</NavLink>
-					
-					<SheetFooter className="place-self-center w-[50%] my-4">
-						<SheetClose asChild>
-							<Button className="w-full bg-secondary">Stäng</Button>
-						</SheetClose>
-					</SheetFooter>
-				</SheetContent>
-			</Sheet>
+			<HamburgerMenu />
 			<nav>
 				<div className="flex space-x-4 text-background text-xl">
-					<NavLink to="/" className="hover:text-muted-foreground ">
-						START
-					</NavLink>
-					<Separator orientation="vertical" className="h-6 self-center bg-muted-foreground" />
-					<NavLink to="/fiske" className="hover:text-muted-foreground ">
-						FISKE
-					</NavLink>
-					<Separator orientation="vertical" className="h-6 self-center bg-muted-foreground" />
-					<NavLink to="/statistik" className="hover:text-muted-foreground ">
-						STATISTIK
-					</NavLink>
-					<Separator orientation="vertical" className="h-6 self-center bg-muted-foreground" />
-					<NavLink to="/boende" className="hover:text-muted-foreground ">
-						BOENDE
-					</NavLink>
-					<Separator orientation="vertical" className="h-6 self-center bg-muted-foreground" />
-					<NavLink to="/bilder" className="hover:text-muted-foreground ">
-						BILDER
-					</NavLink>
+					<NavLinks/>
 				</div>
 			</nav>
 		</header>
