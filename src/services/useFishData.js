a5b720fdbded5a06db80da6c0bf0e60c6a33156b
Igelkotte <jonas.toern@gmail.com>
2024-12-03 17:13:25 +0100

    refactoring: Moved fishdata fetch to seperate file

---
+++ src/services/useFishData.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+function useFishData(year, apiUrl) {
+	const [data, setData] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		const fetchFishDataByYear = async (selectedYear) => {
+			try {
+				setIsLoading(true);
+				setError(null);
+				const response = await fetch(apiUrl + selectedYear);
+
+				if (!response.ok) {
+					throw new Error(`HTTP error! status: ${response.status}`);
+				}
+
+				const responseData = await response.json();
+				const fishData = responseData.Fish || [];
+				setData(fishData);
+				setIsLoading(false);
+			} catch (e) {
+				setError(e.message);
+				setData([]);
+				setIsLoading(false);
+			}
+		};
+
+		fetchFishDataByYear(year);
+	}, [year, apiUrl]);
+
+	return { data, isLoading, error };
+}
+
+export default useFishData;
