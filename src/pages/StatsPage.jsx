191c8f0f0f0db175ec6447b59fd5c4a34b24e1e8
Igelkotte <jonas.toern@gmail.com>
2024-12-03 17:01:01 +0100

    tog bort gammal utkommenterad kod

--- src/pages/StatsPage.jsx
+++ src/pages/StatsPage.jsx
@@ -7,37 +7,7 @@ const StatsPage = () => {
 	
 	return (
 		<div className="w-full max-w-4xl mx-auto mt-8">
-			<FishDataTable></FishDataTable>
-			{/* <Select
-				value={year.toString()}
-				onValueChange={(value) => setYear(Number(value))}
-			>
-				<SelectTrigger className="w-[280px]">
-					<SelectValue placeholder="Välj år:" />
-				</SelectTrigger>
-				<SelectContent className="max-h-[300px] overflow-y-auto">
-					<SelectGroup>
-						<SelectLabel>Årtal</SelectLabel>
-						{years.map((yearOption) => (
-							<SelectItem key={yearOption} value={yearOption.toString()}>
-								{yearOption}
-							</SelectItem>
-						))}
-					</SelectGroup>
-				</SelectContent>
-			</Select>
-
-			<Card className="">
-				<CardHeader>
-					<CardTitle className="flex flex-col space-y-4">
-						<h2>Statistik från {year}</h2>
-					</CardTitle>
-				</CardHeader>
-				<CardContent>
-					
-				</CardContent>
-			</Card>
-			<Separator className="m-4"></Separator> */}
+			<FishDataTable></FishDataTable>			
 		</div>
 	);
 };
