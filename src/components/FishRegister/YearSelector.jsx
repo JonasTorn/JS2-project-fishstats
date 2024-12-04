f13e3db0bdbea295b15fbafee85a42478df9b241
Igelkotte <jonas.toern@gmail.com>
2024-12-04 11:57:33 +0100

    refactoring: Added component for the "year selector"

---
+++ src/components/FishRegister/YearSelector.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "../ui/select";
+
+const YearSelector = ({year, setYear, yearSpan}) => {
+	return (
+		<Select value={year} onValueChange={(value) => setYear(value)}>
+			<SelectTrigger className="w-[280px]">
+				<SelectValue placeholder="Välj år:" />
+			</SelectTrigger>
+			<SelectContent className="max-h-[300px] overflow-y-auto">
+				<SelectGroup>
+					<SelectLabel>Årtal</SelectLabel>
+					{yearSpan.map((yearOption) => (
+						<SelectItem key={yearOption} value={yearOption}>
+							{yearOption}
+						</SelectItem>
+					))}
+				</SelectGroup>
+			</SelectContent>
+		</Select>
+	);
+};
+
+export default YearSelector;
