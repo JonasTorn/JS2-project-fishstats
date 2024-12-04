13eaf36e39db45f8786cfdec0d15a39ec1c25862
Igelkotte <jonas.toern@gmail.com>
2024-12-04 11:56:06 +0100

    refactoring: Added component for column visibility feature

---
+++ src/components/FishRegister/ColumnVisibilityMenu.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { ChevronDown } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
+import {
+	DropdownMenu,
+	DropdownMenuCheckboxItem,
+	DropdownMenuContent,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+const ColumnVisibilityMenu = ({ table }) => {
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button variant="outline" className="ml-auto">
+					Columns <ChevronDown />
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent align="end">
+				{table
+					.getAllColumns()
+					.filter((column) => column.getCanHide())
+					.map((column) => {
+						return (
+							<DropdownMenuCheckboxItem
+								key={column.id}
+								className="capitalize"
+								checked={column.getIsVisible()}
+								onCheckedChange={(value) => column.toggleVisibility(!!value)}
+							>
+								{column.id}
+							</DropdownMenuCheckboxItem>
+						);
+					})}
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
+};
+
+export default ColumnVisibilityMenu;
