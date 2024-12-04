441c617d6fd3a268a76155ed25644d9ab61389cd
Igelkotte <jonas.toern@gmail.com>
2024-12-04 14:26:09 +0100

    Renamed and fixed props in Columns.jsx

--- src/components/FishRegister/Columns.jsx
+++ src/components/FishRegister/Columns.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Columns = ({ data }) => {
+const Columns = ({ data, excludedKeys }) => {
 	if (!data || data.length === 0) return [];
 
-	// Vilka "keys" som INTE ska visas på sidan
-	const keysToExclude = ["id", "created_at", "updated_at", "deleted_at"];
+
+
 	const filteredKeys = Object.keys(data[0] || {}).filter(
-		(key) => !keysToExclude.includes(key)
+		(key) => !excludedKeys.includes(key)
 	);
 
 	// Skapar kolumner
